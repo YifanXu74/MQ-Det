c3dfb8b73a4aaea95d931cb8a74d704d6099a554
YifanXu <yifan.xu@nlpr.ia.ac.cn>
2023-05-31 17:08:25 +0800

    initialize

--- tools/test_grounding_net.py
+++ tools/test_grounding_net.py
@@ -1,4 +1,3 @@
-# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
 # Set up custom environment before nearly anything else is imported
 # NOTE: this should be the first import (no not reorder)
 from maskrcnn_benchmark.utils.env import setup_environment  # noqa F401 isort:skip
